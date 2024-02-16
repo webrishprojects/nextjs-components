05187a84adbb4399fa3fee6b4978131277e96d32
Webrish <159287019+webrishprojects@users.noreply.github.com>
2024-02-16 21:41:58 +0530

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,4 @@
+import ImageUploader from "./components/uploader/Image Uploader/ImageUploader";
+export default function Home() {
+  return <div className="text-2xl flex justify-center">Home Page</div>;
+}
---
+++ src/app/uploadimages/page.js
@@ -0,0 +1,6 @@
+import ImageUploader from "../components/uploader/Image Uploader/ImageUploader";
+const page = () => {
+  return <ImageUploader />;
+};
+
+export default page;
